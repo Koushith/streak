ec551df94aee52e40c89c05ef2fb9f9d98168a8b
koushith <koushith97@gmail.com>
2022-12-08 13:01:59 +0530

    adds google fonts

--- App.tsx
+++ App.tsx
@@ -2,11 +2,55 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  useFonts as useRubik,
+} from "@expo-google-fonts/rubik";
+import {
+  IBMPlexSans_700Bold,
+  IBMPlexSans_500Medium,
+  useFonts as useIBMSans,
+} from "@expo-google-fonts/ibm-plex-sans";
+import {
+  IBMPlexMono_600SemiBold,
+  useFonts as useIBMPlexMono,
+} from "@expo-google-fonts/ibm-plex-mono";
+import {
+  Barlow_600SemiBold,
+  Barlow_700Bold,
+  useFonts as useBarlow,
+} from "@expo-google-fonts/barlow";
+
 import { DashBoardScreen, LoginScreen, RegisterScreen } from "./src/screens";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [barlowLoaded] = useBarlow({
+    Barlow_600SemiBold,
+    Barlow_700Bold,
+  });
+
+  const [ibmPlexMonoLoaded] = useIBMPlexMono({
+    IBMPlexMono_600SemiBold,
+  });
+
+  const [ibmSansLoaded] = useIBMSans({
+    IBMPlexSans_700Bold,
+    IBMPlexSans_500Medium,
+  });
+
+  const [rubikLoaded] = useRubik({
+    IBMPlexSans_700Bold,
+    IBMPlexSans_500Medium,
+  });
+
+  if (!rubikLoaded || !ibmPlexMonoLoaded || !ibmSansLoaded || !barlowLoaded) {
+    console.log("herererere");
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- src/screens/auth/login.screen.tsx
+++ src/screens/auth/login.screen.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export const LoginScreen = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <Text>Login screen</Text>
     </View>
   );
 };
